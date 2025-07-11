bbd5fac6477e2bbc123cbb0d8e1cb438d1a8f346
amazia <amaziakristanto03@gmail.com>
2025-07-12 04:06:46 +0700

    dsafdas

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,7 @@
 module.exports = function (eleventyConfig) {
-  // Menyalin folder statis ke hasil akhir
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js");
-  eleventyConfig.addPassthroughCopy("foto");
-  eleventyConfig.addPassthroughCopy("artikel"); // <-- Pastikan ini ada
-
-  // Mengatur Nunjucks sebagai mesin template
   return {
-    markdownTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
     dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
+      input: "src",
       output: "_site",
     },
   };
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+title: Amazia Kristanto
+layout: layout.njk
+---
+
+# Selamat Datang di Website Amazia Kristanto
+
+Ini adalah website personal Amazia Kristanto yang dibangun menggunakan Eleventy (11ty).
